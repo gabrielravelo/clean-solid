055f70816b17b1252e4a1712085de30dcd68e9c9
Gabriel Ravelo <ravelogabriel2@gmail.com>
2024-11-05 23:07:42 -0400

    cap 27

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -8,19 +8,38 @@ class Product {
         public size: Size    = '',
     ){}
 
+    isProductReady():boolean {
+        for( const key in this ) {
+            switch( typeof this[key] ) {
+                case 'string':
+                    if( (<string>this[key]).length <= 0 ) throw Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if( (<number>this[key]) <= 0 ) throw Error(`${key} is zero`);
+                    break;
+                default:
+                    throw Error(`Type of ${key} is not valid`);
+            }
+        }
+        return true;
+    }
+
     toString() {
         // No Dry
-        if( this.name.length <= 0 ) throw Error('name is empty');
-        if( this.price <= 0 ) throw Error('price is zero');
-        if( this.size.length <= 0 ) throw Error('size is empty');
+        // if( this.name.length <= 0 ) throw Error('name is empty');
+        // if( this.price <= 0 ) throw Error('price is zero');
+        // if( this.size.length <= 0 ) throw Error('size is empty');
 
+        // Dry
+        if( !this.isProductReady() ) return;
+        
         return `${this.name} (${this.price}), ${this.size}`;
     }
 }
 
 (() => {
 
-    const bluePants = new Product('Blue large pants', 10, 'S');
+    const bluePants = new Product('Blue pants is empty', 10, 'S');
     console.log(bluePants.toString());
 })();
 
---
+++ src/clean-code/06-classess-a.ts
@@ -0,0 +1,18 @@
+(() => {
+
+    type Gender = 'M'|'F';
+
+    class Person {
+        constructor(
+            public name:string, 
+            public gender: Gender, 
+            public birthdate: Date
+        ){}
+    }
+
+    const newPerson = new Person('Gabriel', 'M', new Date('1985-10-21'));
+    console.log({newPerson});
+
+
+
+})();
